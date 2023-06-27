3f83dc77dd45a01fc7c852ea6d11eff7cb6614af
Nato Boram <nato.boram@gmail.com>
2023-06-26 20:38:44 -0400

    🚚 Rename to Diverse-Entities-For-Faithful-32x

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,5 +1,5 @@
 @echo off
 if "%CD:~-7%"=="scripts" cd ..
 
-del Alternate-Entities-Faithful-32x.zip
-7z a -mmt6 -mx9 Alternate-Entities-Faithful-32x.zip assets LICENSE.txt pack.mcmeta pack.png
+del Diverse-Entities-For-Faithful-32x.zip
+7z a -mmt6 -mx9 Diverse-Entities-For-Faithful-32x.zip assets LICENSE.txt pack.mcmeta pack.png
--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ if [ $(basename $PWD) = "scripts" ]; then
 	cd ..
 fi
 
-rm Alternate-Entities-Faithful-32x.zip
-7z a -mmt6 -mx9 Alternate-Entities-Faithful-32x.zip assets LICENSE.txt pack.mcmeta pack.png
+rm Diverse-Entities-For-Faithful-32x.zip
+7z a -mmt6 -mx9 Diverse-Entities-For-Faithful-32x.zip assets LICENSE.txt pack.mcmeta pack.png
